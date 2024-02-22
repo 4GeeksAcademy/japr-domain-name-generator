40117da064dd682f318743bf7bc32ef8e3829a78
japr91 <139667967+japr91@users.noreply.github.com>
2024-02-22 01:11:31 +0000

    termine

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let array1 = ["the", "our"];
+  let array2 = ["great", "big"];
+  let array3 = ["jogger", "racoon"];
+
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      for (let k = 0; k < array3.length; k++) {
+        let domain = array1[i] + array2[j] + array3[k] + ".com";
+
+        console.log(domain);
+      }
+    }
+  }
 };
